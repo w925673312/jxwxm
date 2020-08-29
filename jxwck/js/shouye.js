4cf7bdf806c30d91fdab13a2fc238f836cff80be
I superman <925673312@qq.com>
2020-08-29 18:09:36 +0800

    上传了顶部与搜索框

---
+++ jxwck/js/shouye.js
@@ -0,0 +1,48 @@
+$(function(){
+    // 我的酒仙
+    $(".wode").mouseover(function(){
+        $(".wode2").show().slideDown()
+    })
+    $(".wode2").mouseout(function(){
+        $(".wode2").hide().slideUp()
+    })
+    $(".wode2").mouseout(function(){
+        $(".wode2").hide().slideUp()
+    })
+
+    //手机酒仙网
+    $(".top6").mouseover(function(){
+        $(".top6a").show()
+    })
+    $(".top6").mouseout(function(){
+        $(".top6a").hide()
+    })
+    //网站导航
+    $(".top7").mouseover(function(){
+        $(".top7w").show()
+    })
+    $(".top7").mouseout(function(){
+        $(".top7w").hide()
+    })
+    $(".top7w").mouseover(function(){
+        $(".top7w").show()
+    })
+    $(".top7w").mouseout(function(){
+        $(".top7w").hide()
+    })
+
+    //客户服务
+    $(".top8").mouseover(function(){
+        $(".top8b").show()
+    })
+    $(".top8").mouseout(function(){
+        $(".top8b").hide()
+    })
+    $(".top8b").mouseover(function(){
+        $(".top8b").show()
+    })
+    $(".top8b").mouseout(function(){
+        $(".top8b").hide()
+    })
+})
+
